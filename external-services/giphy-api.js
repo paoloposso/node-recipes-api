c9be892f702855e4b9a3bac5f5263b381ab2dde4
Paolo Posso <pvictorsys@gmail.com>
2020-11-29 17:22:29 -0300

    limit gif api number

--- external-services/giphy-api.js
+++ external-services/giphy-api.js
@@ -7,5 +7,5 @@ const axios = require('axios');
 module.exports.getGif = async (searchKey) => {
     const apiKey = process.env.GIPHY_API_KEY;
 
-    return await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchKey}`);
+    return await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchKey}&limit=1`);
 }
